ceb2e156498983d18f46065caaef703ee0eb9925
David Hansen <david.hansen.0000@gmail.com>
2018-09-02 05:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var emptyArray = Array();
-  
+  var musical = new Array(musicians.length);
+  for(var i=0; i<musicians.length; i++) {
+    emptyArray[i]=musicians[i] + " plays "+ instruments[i];
+  }
+  return musical;
+}
+
+function johnLennonFacts(facts) {
+  for (var i=0; i<facts.length; i++) {
+    facts[i] = facts[i]+"!!!";
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  var array=[];
+  do {
+    array.push("I Love the Beatles!");
+    num++;
+  } while(num<15);
 }
